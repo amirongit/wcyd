4a5396415e4a7ba33b2bccd44a542f69ebeaaf50
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-06-05 23:16:49 +0330

    add classes for grpc and http node client impls

---
+++ src/infra/grpc_node_client.py
@@ -0,0 +1,16 @@
+from src.abc.core.inode import INode
+from src.abc.infra.inode_client import INodeClient
+from src.type.dto import ChannelDTO
+from src.type.alias import Identifier
+
+
+class GRPCNodeClient(INodeClient):
+
+    async def get_neighbors(self, channel: ChannelDTO) -> set[Identifier]:
+        raise NotImplemented
+
+    async def connect_node(self, channel: ChannelDTO) -> None:
+        raise NotImplemented
+
+    async def find_node(self, questioners: set[Identifier], identifier: Identifier) -> INode:
+        raise NotImplemented
