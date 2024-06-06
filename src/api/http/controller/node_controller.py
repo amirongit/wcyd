7029d4bd80a79d693a5774d0847b2e93ca363273
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-06-06 10:59:16 +0330

    improve typing int node controller

--- src/api/http/controller/node_controller.py
+++ src/api/http/controller/node_controller.py
@@ -1,7 +1,8 @@
 from blacksheep.server.controllers import post
 from blacksheep import FromJSON, Response
 
-from src.abc.core.inode import INode
+from src.core.local_node import LocalNode
+from src.core.remote_node import RemoteNode
 from src.api.http.controller.base_controller import BaseController
 from src.api.http.io_type.node_io import NodeConnectionRequest
 from src.api.utils import remote_node_factory
@@ -9,16 +10,20 @@ from src.api.utils import remote_node_factory
 
 class NodeController(BaseController):
 
-    ROUTE = '/node'
+    ROUTE = '/nodes'
 
-    def __init__(self, local_node: INode) -> None:
+    def __init__(self, local_node: LocalNode) -> None:
         self._local_node = local_node
 
     @post('/')
     async def connect(self, request_body: FromJSON[NodeConnectionRequest]) -> Response:
 
         serialized = request_body.value
-        remote_node: INode = remote_node_factory(serialized.identifier, serialized.endpoint, serialized.public_key)
+        remote_node: RemoteNode = remote_node_factory(
+            serialized.identifier,
+            serialized.endpoint,
+            serialized.public_key
+        )
         await self._local_node.connect(remote_node)
 
         return self.created()
