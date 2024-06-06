5eabb92a377136eb7c9d9d518123d7541e1e9ec2
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-06-06 10:59:50 +0330

    add global settings

---
+++ src/settings.py
@@ -0,0 +1,21 @@
+from json import loads
+
+from pydantic.dataclasses import dataclass
+from pydantic.networks import AnyUrl
+
+
+@dataclass(kw_only=True)
+class NodeSettings:
+    IDENTIFIER: str
+    PUBLIC_KEY: str
+    ENDPOINT: AnyUrl
+
+
+@dataclass(kw_only=True)
+class Settings:
+    LOCAL_NODE: NodeSettings
+
+
+SETTINGS: Settings
+with open('./settings.json', 'r') as f:
+    SETTINGS = Settings(**loads(f.read()))
