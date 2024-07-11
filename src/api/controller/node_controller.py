1aba4b87bfbf7bd5138d3df2c35a906f810ffc08
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-07-11 12:40:07 +0330

    modify node controller to use usecases intead of services

--- src/api/controller/node_controller.py
+++ src/api/controller/node_controller.py
@@ -2,7 +2,8 @@ from blacksheep import FromJSON, FromRoute, FromQuery, Response
 from blacksheep.server.controllers import post, get
 from blacksheep.server.openapi.common import ContentInfo, ResponseInfo
 
-from src.abc.service.inode_service import INodeService
+from src.abc.use_case.connect_node_use_case import ConnectNodeUseCase
+from src.abc.use_case.find_node_use_case import FindNodeUseCase
 from src.api.controller.base_controller import BaseController
 from src.api.docs import docs
 from src.api.io_type.node_io import NodeConnectionRequest, NodeModel
@@ -12,8 +13,9 @@ class NodeController(BaseController):
 
     ROUTE = '/nodes'
 
-    def __init__(self, node_service: INodeService) -> None:
-        self._node_service = node_service
+    def __init__(self, connect_use_case: ConnectNodeUseCase, find_node_use_case: FindNodeUseCase) -> None:
+        self._connect_use_case = connect_use_case
+        self._find_use_case = find_node_use_case
 
     @docs(
         tags=['nodes'],
@@ -24,7 +26,7 @@ class NodeController(BaseController):
     async def register_node(self, request_body: FromJSON[NodeConnectionRequest]) -> Response:
 
         serialized = request_body.value
-        await self._node_service.connect(serialized.identifier, serialized.endpoint)
+        await self._connect_use_case.execute(serialized.identifier, serialized.endpoint)
 
         return self.created()
 
@@ -40,6 +42,6 @@ class NodeController(BaseController):
     @get('/{identifier}')
     async def get_node(self, identifier: FromRoute[str], questioners: FromQuery[set[str]]) -> Response:
 
-        node = await self._node_service.find(questioners.value, identifier.value)
+        node = await self._find_use_case.execute(questioners.value, identifier.value)
 
         return self.ok(NodeModel(identifier=node.identifier, endpoint=str(node.endpoint)))
