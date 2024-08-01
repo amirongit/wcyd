c937d042d6871f965349e0874c3d9ddc1195b439
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-08-01 12:15:27 +0330

    modify verify util to get bytes from hex values, and make auth work

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
 
-from blacksheep import Request
+from blacksheep import Application, Request
 from guardpost import AuthenticationHandler, Identity
 
+from src.abc.use_case.find_peer_use_case import FindPeerUseCase
 from src.settings import AuthenticationSettings
 from src.type.internal import UniversalPeerIdentifier
-from src.use_case.find_peer import FindPeerUseCase
 from src.utils import EncryptionUtils
 
 
@@ -19,29 +19,35 @@ class DecentralizedAuthenticationHandler(AuthenticationHandler):
         d. if the timestamp is within the range of +/- `settings.AUTHENTICATION.TOKEN_TIME_WINDOW`,
            the peer is authenticated within the scope of the request
 
-    * Authorization header format: Basic Base64.encode(peer@node:signed_timestamp)
+    * Authorization header format: Basic Base64.encode(peer@node:hex(signed_timestamp))
     '''
 
-    def __init__(self, find_peer_use_case: FindPeerUseCase, node_settings: AuthenticationSettings) -> None:
-        self._find_peer_use_case = find_peer_use_case
-        self._settings = node_settings
+    def __init__(self, app: Application) -> None:
+        super().__init__()
+        self.app = app
+
+    @property
+    def _find_peer_use_case(self) -> FindPeerUseCase: return self.app.services.resolve(FindPeerUseCase)
+
+    @property
+    def _settings(self) -> AuthenticationSettings: return self.app.services.resolve(AuthenticationSettings)
 
     async def authenticate(self, context: Request) -> Identity | None: # type: ignore
         context.identity = None
 
         try:
             identifier, signature = EncryptionUtils.decrypt_base64(
-                    context.headers.get(b'Authorization')[0][1].decode().removeprefix('Basic ')
+                context.get_first_header(b'Authorization').decode().removeprefix('Basic ') # type: ignore
             ).split(':')
             peer_identifier, node_identifier = identifier.split('@')
             universal_identifier = UniversalPeerIdentifier(peer=peer_identifier, node=node_identifier)
             peer = await self._find_peer_use_case.execute(universal_identifier)
-            content  = EncryptionUtils.verify_signature(signature, peer.keyring.signing)
+            signed_timestamp  = float(EncryptionUtils.verify_signature(signature, peer.keyring.signing))
             now, window_range = datetime.now(), timedelta(seconds=self._settings.TIME_WINDOW)
             start, end = now - window_range, now + window_range
-            given = datetime.fromtimestamp(int(content)) # type: ignore
+            given = datetime.fromtimestamp(signed_timestamp)
             if given > start and given < end:
-                context.identity = Identity({'id': universal_identifier})
+                context.identity = Identity({'id': universal_identifier}, 'authenticated')
         except Exception:
             pass
 
