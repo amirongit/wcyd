02bcc801edd0e8a945698dde0333ef81b4302302
AmirHossein Ahmadi <pinoq@protonmail.com>
2024-08-02 12:07:06 +0330

    add credentials to send message use case

--- src/abc/use_case/send_message_use_case.py
+++ src/abc/use_case/send_message_use_case.py
@@ -1,9 +1,15 @@
 from abc import ABC, abstractmethod
 
-from src.type.internal import UniversalPeerIdentifier
+from src.type.internal import PeerCredentials, UniversalPeerIdentifier
 
 
 class SendMessageUseCase(ABC):
 
     @abstractmethod
-    async def execute(self, source: UniversalPeerIdentifier, target: UniversalPeerIdentifier, content: str) -> None: ...
+    async def execute(
+        self,
+        source: UniversalPeerIdentifier,
+        target: UniversalPeerIdentifier,
+        content: str,
+        credentials: PeerCredentials
+    ) -> None: ...
